74f19a8feb34cf46eb8cd4893d823491f0c3d6de
Diego Tamayo <diegotamayo@Diegos-MacBook-Pro-2.local>
2025-10-31 17:28:35 -0400

    Clean up: remove old outer folder and keep correct project structure

--- literateApp/sqlalchemy_helpers.py
+++ literateApp/sqlalchemy_helpers.py
@@ -1,24 +1,55 @@
 from sqlalchemy import create_engine, engine
 
 
-def sqlalchemy_db_init(driver_name, user, password, host, port, db_name, pool_size=5, max_overflow=2, pool_timeout=60,
-                       pool_recycle=1800):
+# def sqlalchemy_db_init(driver_name, user, password, host, port, db_name, pool_size=5, max_overflow=2, pool_timeout=60,
+#                        pool_recycle=1800):
+#     pool = create_engine(
+#         engine.url.URL.create(
+#             drivername=driver_name,
+#             username=user,
+#             password=password,
+#             host=host,
+#             port=port,
+#             database=db_name,
+#         ),
+#         pool_size=pool_size,
+#         max_overflow=max_overflow,
+#         pool_timeout=pool_timeout,
+#         pool_recycle=pool_recycle,
+#     )
+#     return pool
+
+
+def sqlalchemy_db_init(
+    driver_name,
+    user,
+    password,
+    host,
+    port,
+    db_name,
+    pool_size=5,
+    max_overflow=2,
+    pool_timeout=60,
+    pool_recycle=1800,
+):
+    url = engine.URL.create(
+        drivername=driver_name,
+        username=user,
+        password=password,
+        host=host,
+        port=port,
+        database=db_name,
+        query={"sslmode": "require"}  # <-- add this
+    )
+
     pool = create_engine(
-        engine.url.URL.create(
-            drivername=driver_name,
-            username=user,
-            password=password,
-            host=host,
-            port=port,
-            database=db_name,
-        ),
+        url,
         pool_size=pool_size,
         max_overflow=max_overflow,
         pool_timeout=pool_timeout,
         pool_recycle=pool_recycle,
     )
     return pool
-
 
 def return_dict(query):
     return [q.__dict__ for q in query]
