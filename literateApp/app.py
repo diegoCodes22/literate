5b596398c5af04b393480897bee09d9e30f5c40e
Diego Tamayo <diegotamayo@Diegos-MacBook-Pro-2.local>
2025-11-01 09:25:39 -0400

    1

--- literateApp/app.py
+++ literateApp/app.py
@@ -38,7 +38,7 @@ CORS(app)
 
 # Configure session to use filesystem instead of signed cookies
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "null"
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Connect to database
